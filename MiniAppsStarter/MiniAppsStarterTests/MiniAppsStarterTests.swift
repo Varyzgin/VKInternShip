5cd5c1f719d67f3b06dc87b1d2d6efb898914a3d
Varyzgin <dima5varyzgin@gmail.com>
2024-09-05 16:03:18 +0300

    New App

---
+++ MiniAppsStarter/MiniAppsStarterTests/MiniAppsStarterTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiniAppsStarterTests.swift
+//  MiniAppsStarterTests
+//
+//  Created by Дима on 9/5/24.
+//
+
+import Testing
+@testable import MiniAppsStarter
+
+struct MiniAppsStarterTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
